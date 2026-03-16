ffd35740a4df93fde30105156cbabe9c0192c855
v-cut127 <violakr2016@gmail.com>
2026-03-16 22:35:37 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,12 @@ import cors from "cors";
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 
+//import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
 import classesRouter from "./routes/classes.js";
+//import departmentsRouter from "./routes/departments.js";
+//import statsRouter from "./routes/stats.js";
+//import enrollmentsRouter from "./routes/enrollments.js";
 
 // import securityMiddleware from "./middleware/security.js";
 import { auth } from "./lib/auth.js";
@@ -17,9 +21,9 @@ const PORT = 8000;
 
 app.use(
     cors({
-      origin: process.env.FRONTEND_URL, // React app URL
-      methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-      credentials: true, // allow cookies
+        origin: process.env.FRONTEND_URL, // React app URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+        credentials: true, // allow cookies
     })
 );
 
@@ -27,14 +31,19 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
+// app.use(securityMiddleware);
 
+//app.use("/api/subjects", subjectsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/classes", classesRouter);
+//app.use("/api/departments", departmentsRouter);
+//app.use("/api/stats", statsRouter);
+//app.use("/api/enrollments", enrollmentsRouter);
 
 app.get("/", (req, res) => {
-  res.send("Backend server is running!");
+    res.send("Backend server is running!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
